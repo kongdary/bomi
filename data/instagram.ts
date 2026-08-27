4f71cdb2260df380889480ab97146b5147b04636
kongdary <agfe6981@gmail.com>
2026-08-27 10:58:49 +0900

    Replace broken Instagram reel

--- data/instagram.ts
+++ data/instagram.ts
@@ -4,5 +4,5 @@ export const instagramPosts: InstagramPost[] = [
   { url: 'https://www.instagram.com/reel/DXrkUQ7EYpj/', type: 'reel', date: '2026-01-01' },
   { url: 'https://www.instagram.com/reel/DYHgBYHxgS1/', type: 'reel', date: '2026-01-02' },
   { url: 'https://www.instagram.com/reel/DMF943VhgDI/', type: 'reel', date: '2025-07-18' },
-  { url: 'https://www.instagram.com/reel/DFWyVK4SS9h/', type: 'reel', date: '2025-02-03' },
+  { url: 'https://www.instagram.com/reel/DRh1dEAgRZU/', type: 'reel', date: '2025-11-28' },
 ];
